5d8154ea76299da80c7e608862c77a5e7253fefe
Andrew Glenn <drstrangembm@gmail.com>
2016-01-18 08:25:45 -0600

    first committing assignment

---
+++ ObjectAssessment.js
@@ -0,0 +1,12 @@
+avengersMovie = {
+	name : "Avengers",
+	runtime : 143 + " mins",
+	releaseYear : 2012,
+	Director : "Joss Whedon"
+};
+
+function showObject(){
+	return avengersMovie;
+};
+
+console.log(showObject());
